643cde6a6c519e3693b297832f45df97978e907b
joeltok <jttctc@gmail.com>
2021-11-17 02:13:30 +0800

    draw all

--- camunda-service-tasks/results-analysis/index.py
+++ camunda-service-tasks/results-analysis/index.py
@@ -5,11 +5,14 @@ import json
 import os
 from pprint import pprint
 
+experiment_results_folder = '../experiment-results'
+experiment_results_folder = '../experiment-results-w-tomcat-limit'
+
 # get all results
 results = []
-result_files = os.listdir('../experiment-results')
+result_files = os.listdir(experiment_results_folder)
 for result_file in result_files:
-  with open(f'../experiment-results/{result_file}') as f:
+  with open(f'{experiment_results_folder}/{result_file}') as f:
     run = json.load(f)
     results.append({
       "tag": run['tag'],
@@ -24,11 +27,25 @@ for result in results:
     keyed[result['tag']] = {}
   keyed[result['tag']][result['concurrent']] = result
 
+# def generate_boxplot(
+#   name="all_boxplots",
+#   rows=['1', '10', '100'],
+#   cols=[
+#     'Basic Java \nClass',
+#     'Basic Node \nTask Client - \n1 worker',
+#     'Basic Node \nTask Client - \n2 workers',
+#     'Advanced Java \nClass',
+#     'Advanced Node \nTask Client - \n1 worker',
+#     'Advanced Node \nTask Client - \n2 workers',
+#   ],
+
+# )
+
 # ===== Comparing All =====
 # boxplot durations
 # dimensions: tag and concurrency
 
-rows = ['1       ', '10      ', '100     ']
+rows = ['1', '10', '100']
 cols = [
   'Basic Java \nClass',
   'Basic Node \nTask Client - \n1 worker',
@@ -44,7 +61,7 @@ for ax, col in zip(axs[0], cols):
 
 for ax, row in zip(axs[:,0], rows):
     ax.set_ylabel(row, rotation=0, size='large')
-    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000) + 's'))
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f}'.format(x/1000/60) + 'mins'))
 
 fig.tight_layout()
 
@@ -58,25 +75,61 @@ axs[0,1].boxplot(keyed['basic-node-task-client-single-worker']['1']['allDuration
 axs[1,1].boxplot(keyed['basic-node-task-client-single-worker']['10']['allDurationsInMillis'], showfliers=False)
 axs[2,1].boxplot(keyed['basic-node-task-client-single-worker']['100']['allDurationsInMillis'], showfliers=False)
 
-# axs[0,2].boxplot(keyed['basic-node-task-client-double-worker']['1']['allDurationsInMillis'], showfliers=False)
-# axs[1,2].boxplot(keyed['basic-node-task-client-double-worker']['10']['allDurationsInMillis'], showfliers=False)
-# axs[2,2].boxplot(keyed['basic-node-task-client-double-worker']['100']['allDurationsInMillis'], showfliers=False)
+axs[0,2].boxplot(keyed['basic-node-task-client-double-worker']['1']['allDurationsInMillis'], showfliers=False)
+axs[1,2].boxplot(keyed['basic-node-task-client-double-worker']['10']['allDurationsInMillis'], showfliers=False)
+axs[2,2].boxplot(keyed['basic-node-task-client-double-worker']['100']['allDurationsInMillis'], showfliers=False)
 
 axs[0,3].boxplot(keyed['advanced-java-class']['1']['allDurationsInMillis'], showfliers=False)
 axs[1,3].boxplot(keyed['advanced-java-class']['10']['allDurationsInMillis'], showfliers=False)
 axs[2,3].boxplot(keyed['advanced-java-class']['100']['allDurationsInMillis'], showfliers=False)
 
-# axs[0,4].boxplot(keyed['advanced-node-task-client-single-worker']['1']['allDurationsInMillis'], showfliers=False)
-# axs[1,4].boxplot(keyed['advanced-node-task-client-single-worker']['10']['allDurationsInMillis'], showfliers=False)
-# axs[2,4].boxplot(keyed['advanced-node-task-client-single-worker']['100']['allDurationsInMillis'], showfliers=False)
+axs[0,4].boxplot(keyed['advanced-node-task-client-single-worker']['1']['allDurationsInMillis'], showfliers=False)
+axs[1,4].boxplot(keyed['advanced-node-task-client-single-worker']['10']['allDurationsInMillis'], showfliers=False)
+axs[2,4].boxplot(keyed['advanced-node-task-client-single-worker']['100']['allDurationsInMillis'], showfliers=False)
 
-# axs[0,5].boxplot(keyed['advanced-node-task-client-double-worker']['1']['allDurationsInMillis'], showfliers=False)
-# axs[1,5].boxplot(keyed['advanced-node-task-client-double-worker']['10']['allDurationsInMillis'], showfliers=False)
-# axs[2,5].boxplot(keyed['advanced-node-task-client-double-worker']['100']['allDurationsInMillis'], showfliers=False)
+axs[0,5].boxplot(keyed['advanced-node-task-client-double-worker']['1']['allDurationsInMillis'], showfliers=False)
+axs[1,5].boxplot(keyed['advanced-node-task-client-double-worker']['10']['allDurationsInMillis'], showfliers=False)
+axs[2,5].boxplot(keyed['advanced-node-task-client-double-worker']['100']['allDurationsInMillis'], showfliers=False)
 
 plt.savefig('./all_boxplots.png')
 
+# # ===== All in View =====
 
-# ===== Examine Node Task Client worker parallelism =====
+# # ===== 1st Subsection - 1 Concurrent across all 6 =====
 
-# Suspicion: network latency / ping
+# # ===== 2nd Subsection - 1 & 10 Concurrent across all 6 =====
+# # - Java Class the same
+# # - Progressively higher
+
+# # ===== 3rd Subsection - 100 across all 6 =====
+# # - Possible throttling because of max connections
+
+
+# # ===== Comparing with possible latency effects =====
+# keyed['basic-java-class']['1']['allDurationsInMillis']
+# keyed['basic-node-task-client-single-worker']['1']['allDurationsInMillis']
+# # have to compare against a java tomcat server
+
+
+
+# # ===== Examine Node Task Client worker parallelism =====
+# keyed['basic-node-task-client-single-worker']['100']['allDurationsInMillis']
+# keyed['basic-node-task-client-double-worker']['100']['allDurationsInMillis']
+
+# keyed['advanced-node-task-client-single-worker']['1']['allDurationsInMillis']
+# keyed['advanced-node-task-client-single-worker']['10']['allDurationsInMillis']
+# keyed['advanced-node-task-client-single-worker']['100']['allDurationsInMillis']
+# keyed['advanced-node-task-client-double-worker']['1']['allDurationsInMillis']
+# keyed['advanced-node-task-client-double-worker']['10']['allDurationsInMillis']
+# keyed['advanced-node-task-client-double-worker']['100']['allDurationsInMillis']
+# -> very prominent
+
+
+
+# ===== Without rate limiting on the Tomcat server =====
+# Suspicion: network latency / ping (even on localhost?)
+# Built in Tomcat rate limit through the Semaphore Valve (limit 10 concurrent)
+
+
+
+# End with: Any other tradeoff considerations that could be worth looking at? Drop me a note in the comments below!
